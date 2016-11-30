3e44c15a4ddc800b03d36dfe9f3fa492a09cc69d
Tina Schönborn <tina.schoenborn@student.htw-berlin.de>
2016-11-30 18:40:58 +0100

    create second activity and rename first

--- app/src/main/java/mobanwendungen/shoppinglist/ShoppingItemActivity.java
+++ app/src/main/java/mobanwendungen/shoppinglist/ShoppingItemActivity.java
@@ -3,11 +3,11 @@ package mobanwendungen.shoppinglist;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ShoppingItemActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_shopping_item);
     }
 }
