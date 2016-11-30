d28d6686edceaa46c515f1d28e4d4392ac275802
Tina Schönborn <tina.schoenborn@student.htw-berlin.de>
2016-11-30 18:41:21 +0100

    set up basic database helper

---
+++ app/src/main/java/mobanwendungen/shoppinglist/database/ShoppinglistTable.java
@@ -0,0 +1,35 @@
+package mobanwendungen.shoppinglist.database;
+
+
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+public class ShoppinglistTable {
+
+    public static final String TABLE_SHOPPINGLIST = "shoppinglist";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_CATEGORY = "category";
+    public static final String COLUMN_SUMMARY = "summary";
+    public static final String COLUMN_DESCRIPTION = "description";
+
+    private static final String DATABASE_CREATE = "create table"
+            + TABLE_SHOPPINGLIST
+            + "("
+            + COLUMN_ID + "integer primary key autoincrement"
+            + COLUMN_CATEGORY + "text not null"
+            + COLUMN_SUMMARY + "text not null"
+            + COLUMN_DESCRIPTION + "text not null"
+            + ");";
+
+    public static void onCreate(SQLiteDatabase database) {
+        database.execSQL(DATABASE_CREATE);
+    }
+
+    public static void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
+        Log.w(ShoppinglistTable.class.getName(),
+                "Upgrading database from version " + oldVersion + " to " + newVersion
+                + ", which will destroy all old data");
+        database.execSQL("DROP TABLE IF EXISTS " + TABLE_SHOPPINGLIST + ";");
+        onCreate(database);
+    }
+}
