bbf1761fb3d290303e9450b1ef590c2eafe87057
Tina Schönborn <tina.schoenborn@student.htw-berlin.de>
2016-12-29 18:36:03 +0100

    add new shoppingitem table

--- app/src/main/java/mobanwendungen/shoppinglist/database/ShoppinglistDatabaseHelper.java
+++ app/src/main/java/mobanwendungen/shoppinglist/database/ShoppinglistDatabaseHelper.java
@@ -7,7 +7,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class ShoppinglistDatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "shoppinglisttable.db";
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
 
     public ShoppinglistDatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -16,12 +16,13 @@ public class ShoppinglistDatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase database) {
         ShoppinglistTable.onCreate(database);
-
+        ShoppingItemTable.onCreate(database);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
         ShoppinglistTable.onUpgrade(database, oldVersion, newVersion);
+        ShoppingItemTable.onUpgrade(database, oldVersion, newVersion);
     }
 }
 
