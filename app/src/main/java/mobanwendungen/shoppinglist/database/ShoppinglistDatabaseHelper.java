daaabaea7bc1e092cd0b10dd65d546de5e65cca4
Tina Schönborn <tina.schoenborn@student.htw-berlin.de>
2017-01-07 14:49:07 +0100

    update database version

--- app/src/main/java/mobanwendungen/shoppinglist/database/ShoppinglistDatabaseHelper.java
+++ app/src/main/java/mobanwendungen/shoppinglist/database/ShoppinglistDatabaseHelper.java
@@ -7,7 +7,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class ShoppinglistDatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "shoppinglisttable.db";
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 3;
 
     public ShoppinglistDatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
